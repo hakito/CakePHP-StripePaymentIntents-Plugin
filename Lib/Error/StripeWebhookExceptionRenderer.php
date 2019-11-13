9c288959704fc86e6904642e452dddbff90fe8a4
gerd <gerd.katzenbeisser@gmail.com>
2019-11-13 15:15:03 +0100

    Error codes in custom renderer

--- Lib/Error/StripeWebhookExceptionRenderer.php
+++ Lib/Error/StripeWebhookExceptionRenderer.php
@@ -4,6 +4,9 @@ App::uses('ExceptionRenderer', 'Error');
 
 class StripeWebhookExceptionRenderer extends ExceptionRenderer {
     public function badRequest($error) {
-        echo $error->getMessage();
+        $this->controller->response->statusCode($error->getCode());
+        $this->controller->layout = 'plain';
+        $this->controller->set('message', $error->getMessage());
+        $this->_outputMessage('plain');
     }
 }
